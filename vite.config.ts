c4fb2ae8c4a330e9bc1ed14e43a45c6404584df5
jphillips356 <jdphil23462@gmail.com>
2024-11-01 19:17:42 -0400

    added start to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Enable external access
+    port: 5173        // Set the port to your desired one (5173 by default)
+  },
+});
