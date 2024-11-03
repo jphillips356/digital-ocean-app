27a9fe8d478f7814df0169c0e0539001f2250b09
Adrian <acozy03@gmail.com>
2024-11-03 12:31:47 -0500

    Push

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
