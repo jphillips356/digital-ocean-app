a4d317b9a1968c2a4ee7ec22875e256447e895c8
jphillips356 <jdphil23462@gmail.com>
2024-11-05 21:41:52 -0500

    adding a home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,37 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './App.css';
+
+// import LoginPage from './pages/LoginPage';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<LoginPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//       );
+// }
+
+// export default App;
+
+// src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
-      );
+  );
 }
 
 export default App;
+
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+// src/pages/HomePage.tsx
+//import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to the Home Page</h1>
+      <p>You have successfully logged in!</p>
+      {/* Add more content here as needed */}
+    </div>
+  );
+}
+
+export default HomePage;
