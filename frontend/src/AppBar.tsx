fe66c011570f581b9347fd4067a4c861a3028049
Adrian <acozy03@gmail.com>
2024-11-26 14:17:42 -0500

    image changes

--- frontend/src/AppBar.tsx
+++ frontend/src/AppBar.tsx
@@ -11,7 +11,7 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 
 // Example logo image, replace with your actual logo path
-import logo from '../tracktion.png';
+import logo from 'src/assets/tracktion.png';
 
 const pages = ['About us'];
 
